7426a9f075606655562ab39b91eefba2f74e61d7
Concrete18 <michaelericson19@gmail.com>
2020-08-17 17:27:57 -0400

    Finished script so it does its main function with some limits.

--- Shared-Shared-Game-Checker.py
+++ Shared-Shared-Game-Checker.py
@@ -8,7 +8,7 @@ def Get_Game_Names(steam_id):
         steam_id = input('Invalid Steam ID (It must be 17 numbers.)\nTry Again.\n')
     with open('api_key.txt') as f:
         api_key = f.read()
-    base_url = f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={steam_id}&format=json&include_appinfo=1'
+    base_url = f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={steam_id}&include_played_free_games=0&format=json&include_appinfo=1'
     data = requests.get(base_url).json()
     game_list = []
     for item in data['response']['games']:
@@ -34,19 +34,20 @@ def Check_For_Shared_Games(lists_to_check):
 def Create_Game_Lists():
     user_check_count = int(input('How many users do you want to compare games for? (2-4)\n'))
     lists_to_check = []
-    for user in range(1, user_check_count):
-        if user_check_count > 1:
-            list_1 = Get_Game_Names(input('\nInput Steam ID 1\n'))
-            list_2 = Get_Game_Names(input('Input Steam ID 2\n'))
-            lists_to_check.append(list_1)
-            lists_to_check.append(list_2)
-        if user_check_count > 2:
-            list_3 = Get_Game_Names(input('Input Steam ID 3\n'))
-            lists_to_check.append(list_3)
-        if user_check_count > 3:
-            list_4 = Get_Game_Names(input('Input Steam ID 4\n'))
-            lists_to_check.append(list_4)
-    print(Check_For_Shared_Games(lists_to_check))
+    if user_check_count > 1:
+        list_1 = Get_Game_Names(input('\nInput Steam ID 1\n'))
+        list_2 = Get_Game_Names(input('Input Steam ID 2\n'))
+        lists_to_check.append(list_1)
+        lists_to_check.append(list_2)
+    if user_check_count > 2:
+        list_3 = Get_Game_Names(input('Input Steam ID 3\n'))
+        lists_to_check.append(list_3)
+    if user_check_count > 3:
+        list_4 = Get_Game_Names(input('Input Steam ID 4\n'))
+        lists_to_check.append(list_4)
+    final_list = Check_For_Shared_Games(lists_to_check)
+    print(', '.join(final_list))
+    input()
 
 
 if __name__ == "__main__":
